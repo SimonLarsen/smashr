804e82503279eeb6ab546c0b5468cd0d6b07584f
Simon Larsen <simonhffh@gmail.com>
2018-06-12 11:22:18 +0200

    Added max.depth argument

--- R/smash.R
+++ R/smash.R
@@ -1,24 +1,25 @@
 #' Smash a nested list into a tidy table.
 #'
 #' @param x List to smash.
-#' @param Name for column corresponding to each level.
+#' @param Name Name for column corresponding to each level.
+#' @param max.depth Maximum number of recursive levels to flatten.
 #' @return A \code{data.table} object.
 #'
 #' @importFrom data.table data.table
 #' @importFrom data.table rbindlist
 #' @export
-smash <- function(x, col.names=NULL) {
-  out <- smash_internal(x, col.names, 1)
+smash <- function(x, col.names=NULL, max.depth=Inf) {
+  out <- smash_internal(x, col.names, 1, max.depth)
   colnames(out) <- make.unique(colnames(out))
   return(out)
 }
 
-smash_internal <- function(x, col.names, depth) {
-  if(any(sapply(x, class) != "list")) {
+smash_internal <- function(x, col.names, depth, max.depth) {
+  if(depth >= max.depth || any(sapply(x, class) != "list")) {
     return(data.table(rbind(x)))
   }
 
-  out <- lapply(x, smash_internal, col.names[-1], depth+1)
+  out <- lapply(x, smash_internal, col.names[-1], depth+1, max.depth)
   row_counts <- sapply(out, nrow)
 
   out <- rbindlist(out, use.names=TRUE, fill=TRUE)
