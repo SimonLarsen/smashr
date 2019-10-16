25db4858f8a93773694aa728666705627c0ecbfe
Simon Larsen <simonhffh@gmail.com>
2019-10-16 19:52:29 +0200

    Replace data.table with tibble/dplyr

--- R/smash.R
+++ R/smash.R
@@ -5,27 +5,28 @@
 #' @param max.depth Maximum number of recursive levels to flatten.
 #' @return A \code{data.table} object.
 #'
-#' @importFrom data.table data.table
-#' @importFrom data.table rbindlist
+#' @importFrom tibble tibble
+#' @importFrom tibble as_tibble
+#' @importFrom dplyr bind_rows
 #' @export
 smash <- function(x, col.names=NULL, max.depth=Inf) {
-  out <- smash_internal(x, col.names, 1, max.depth)
+  out <- .smash_internal(x, col.names, 1, max.depth)
   colnames(out) <- make.unique(colnames(out))
   return(out)
 }
 
-smash_internal <- function(x, col.names, depth, max.depth) {
+.smash_internal <- function(x, col.names, depth, max.depth) {
   if(depth >= max.depth || any(sapply(x, class) != "list")) {
-    return(data.table(rbind(x)))
+    return(as_tibble(x))
   }
 
-  out <- lapply(x, smash_internal, col.names[-1], depth+1, max.depth)
+  out <- lapply(x, .smash_internal, col.names[-1], depth+1, max.depth)
   row_counts <- sapply(out, nrow)
 
-  out <- rbindlist(out, use.names=TRUE, fill=TRUE)
+  out <- bind_rows(out)
 
   if(!is.null(names(x))) {
-    out <- data.table(rep(names(x), row_counts), out)
+    out <- cbind(tibble(rep(names(x), row_counts)), out)
 
     if(!is.null(col.names)) {
       colnames(out)[1] <- col.names[1]
